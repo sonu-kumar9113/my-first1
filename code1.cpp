fea90ac38f951b6e4fafa18580142baf3141641b
SONU KUMAR <sonukumarsonubth@gmail.com>
2024-06-08 19:53:58 +0530

    It is my first commit

---
+++ code1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enetr your number: ";
+    cin>>n;
+    for(int i=0; i<n; i++){
+        cout<<" * ";
+    }
+    return 0;
+}
